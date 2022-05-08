a0634241dfe5a587f2ce3bdff6d3d16e013358a5
eddiebquinn <github@pranonym.com>
2022-05-08 13:36:34 +0100

    various fixes

--- utils.py
+++ utils.py
@@ -11,20 +11,19 @@ class TimeConverter(commands.Converter):
 
         time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
         time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
-
         args = argument.lower()
         matches = re.findall(time_regex, args)
-        time_ = 0
+        time = 0
         for v, k in matches:
             try:
-                time_ += time_dict[k] * float(v)
+                time += time_dict[k] * float(v)
             except KeyError:
                 raise commands.BadArgument(
                     "{} is an invalid time-key! h/m/s/d are valid!".format(k)
                 )
             except ValueError:
                 raise commands.BadArgument("{} is not a number!".format(v))
-        return time_
+        return time
 
 def extract_json():
     file_ = open("settings.json")
