b9fa9bea158557651c4fb80c2fc1040da13aa062
eddie <coding_dir+github@aleeas.com>
2022-05-09 19:43:37 +0100

    fix: :bug: Addressed !relapse not responding correctly to invalid arguments
    Addressed !relapse not responding correctly to invalid arguments. Fixes issue #2

--- utils.py
+++ utils.py
@@ -7,27 +7,25 @@ import re
 
 class TimeConverter(commands.Converter):
 
-    async def convert(self, ctx, argument):
-
-        time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
-        time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
-        args = argument.lower()
-        matches = re.findall(time_regex, args)
-        if len(matches) < 1:
-            await ctx.send("Invalid time-key! h/m/s/d are valid!")
-            return None
+    async def convert(self, ctx, arguments):
+        time_dict = {"s":1, "m":60, "h":3600, "d":86400}
+        if "M" in arguments:
+            await ctx.send("please use 'm' istead of 'M'")
+            return False
+        args = arguments.lower().split()
         time = 0
-        for v, k in matches:
-            try:
-                time += time_dict[k] * float(v)
-            except KeyError:
-                message = f"{k} is an invalid time-key! h/m/s/d are valid!"
-                raise commands.BadArgument(message)
-                return None
-            except ValueError:
-                message = f"{v} is not a number"
-                raise commands.BadArgument(v)
-                return None
+        message = f"You can only use seconds, minutes, hours, and days as time keys."
+        for arg in args:
+            part = re.split('(\d+)',arg)
+            if part[0] != "":
+                await ctx.send(message)
+                return False
+            part.pop(0)
+            if part[1] not in list(time_dict.keys()):
+                await ctx.send(message)
+                return False
+            unit = time_dict[part[1]]
+            time += unit * int(part[0])
         return time
 
 def extract_json():
