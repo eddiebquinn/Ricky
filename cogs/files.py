fcf15b12b8b932793d4f4386a3bf3164c206e18a
Wushi <84910366+WushiSushi@users.noreply.github.com>
2022-05-13 18:15:56 +0530

    Update files.py

--- cogs/files.py
+++ cogs/files.py
@@ -1,6 +1,6 @@
 #Contains media files. Created by Wushi.
 
-moti = [
+motiv = [
   "https://www.youtube.com/watch?v=xBvPPW5uYVQ",
   "https://cdn.shopify.com/s/files/1/0070/7032/files/Fearless_Motivational_Quote_Desktop_Wallpaper_1.png?format=jpg&quality=90&v=1600450412",
   "https://www.success.com/wp-content/uploads/legacy/sites/default/files/new2.jpg",
