15005eaa5d52767f5e2d06e81c98e8755ba29aa0
eddiebquinn <github@pranonym.com>
2022-06-13 05:55:32 +0100

    fix: Added fix for "bool is not scritable" error
    There was a persistant error for a while due to code in relapse, the time converter function in utils returns a tuple, however in the original code it only returned  a bool. Change required me to ensure that essentially a tuple was returned in all cases whent he command fails

--- utils/utils.py
+++ utils/utils.py
@@ -25,7 +25,7 @@ class TimeConverter(commands.Converter):
         time_dict = {"s": 1, "m": 60, "h": 3600, "d": 86400}
         if "M" in arguments:
             await ctx.send("please use 'm' istead of 'M'")
-            return False
+            return (False, overide)
 
         time = 0
         message = f"You can only use seconds, minutes, hours, and days as time keys. \n e.g. - `1d 4h 5m` for 1d 4 hours and 5m"
@@ -33,11 +33,11 @@ class TimeConverter(commands.Converter):
             part = re.split('(\d+)', arg)
             if part[0] != "":
                 await ctx.send(message)
-                return False
+                return (False, overide)
             part.pop(0)
             if part[1] not in list(time_dict.keys()):
                 await ctx.send(message)
-                return False
+                return (False, overide)
             unit = time_dict[part[1]]
             time += unit * int(part[0])
         return (float(time), overide)
